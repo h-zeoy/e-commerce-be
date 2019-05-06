e55d607cfbc57dbbc02141fce6a95b9f22e65fd5
haoxinya <haoxinya@babyfs.cn>
2019-05-06 09:45:11 +0800

    day02

--- controllers/list.js
+++ controllers/list.js
@@ -1,9 +1,10 @@
 const listModel = require('../models/list');
 //显示全部商品
 const listAll = async (req, res, next) =>{
-    res.header('Content-Type', 'application/json; charset=utf-8')
+    res.header('Content-Type', 'application/json; charset=utf-8');
     try {
         let result = await listModel.listAll("select * from list");
+        console.log(result);
         res.render('list', {
             success: true,
             data: JSON.stringify({
@@ -20,8 +21,41 @@ const listAll = async (req, res, next) =>{
         });
     }
 }
-//创建商品
+
+//添加商品
+const addGoods = async(req, res, next) => {
+    let goodsId = 2;
+    let name = '孕妇装';
+    let price = 89.9;
+    let linePrice = 109.9;
+    let channel = 1;
+    let stock = 10;
+    let code = '00000002';
+    let sql = `
+    insert into list(goodsId, name, price, linePrice, channel, stock, code) 
+    value (${goodsId}, "${name}", ${price}, ${linePrice}, ${channel}, ${stock}, "${code}")
+    `;
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    try {
+        await listModel.addGoods(sql);
+        res.render('list', {
+            success: true,
+            data: JSON.stringify({
+                msg: '数据添加成功 :)'
+            })
+        });
+    } catch (err) {
+        console.log(err);
+        res.render('list', {
+            success: false,
+            data: JSON.stringify({
+                msg: '数据添加失败 :('
+            })
+        });
+    }
+}
 
 module.exports = {
-    listAll
+    listAll,
+    addGoods
 }
--- models/list.js
+++ models/list.js
@@ -1,5 +1,6 @@
 const pool = require('../utils/database');
 
+//显示 所有的数据
 const listAll = (sql) => {
     return new Promise((resolve, reject) => {
         pool.getConnection((err, connection) => {
@@ -12,6 +13,22 @@ const listAll = (sql) => {
         return result
     })
 }
+
+//添加数据
+const addGoods = (sql) => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            err ? reject(err) : connection.query(sql, (err, result) => {
+                err ? reject(err) : resolve(result);
+                connection.release();
+            })
+        })
+    }).then((result) => {
+        return result
+    })
+}
+
 module.exports = {
-    listAll
+    listAll,
+    addGoods
 }
